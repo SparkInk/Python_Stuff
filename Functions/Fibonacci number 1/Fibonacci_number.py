c755599ebd7b880298c468a4b0d1d5b9a7a50789
SparkInk <32857343+SparkInk@users.noreply.github.com>
2017-10-17 22:11:27 +0900

    Delete Fibonacci_number.py

--- Functions/Fibonacci number 1/Fibonacci_number.py
+++
@@ -1,8 +0,0 @@
-#it's for python3
-def fib(j):      # here j is a number up to 
-	a = 0    # you want to display the series   
-	b = 1
-	while a < j:
-		print(a, b, end = " ")
-		a = a + b   # this step is changing possitions 
-		b = a + b   # of the parameters a and b in the series
