670aefffafd6bc0e239694332dc3733420775776
SparkInk <32857343+SparkInk@users.noreply.github.com>
2017-10-19 20:01:34 +0900

    Add files via upload

---
+++ Functions/Max_number.py
@@ -0,0 +1,27 @@
+import random
+m = input('A way to fill sequence: 1 -> random or 2 -> by yourself?\n')
+if m == '1':
+    n = int(input('Enter a number up to random seq:\n'))
+    L = list()
+    max = 0
+    for i in range(0, n):
+        L.append(random.randint(0, n ** 2))
+    for i in range(0, len(L)):
+        if L[i] > max:
+            max = L[i]
+    print('The sequence is', L)
+    print('Max number in seq is ', max)
+elif m == '2':
+    L = list()
+    n = int(input('How many numbers you are going to enter?\n'))
+    for i in range(0, n):
+        L.append(int(input('Enter number:\n ')))
+    max = 0
+    for i in range(0, len(L)):
+        if L[i] > max:
+            max = L[i]
+    print('The sequence is', L)
+    print('Max number in seq is', max)
+else: print('Wrong number. Please try again')
+
+ 
