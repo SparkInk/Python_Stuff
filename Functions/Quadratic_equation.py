ae0812bb4e341ce28a69e4b2997c13b29aa2d6df
SparkInk <32857343+SparkInk@users.noreply.github.com>
2017-10-20 20:29:49 +0900

    Update Quadratic_equation.py

--- Functions/Quadratic_equation.py
+++ Functions/Quadratic_equation.py
@@ -1,10 +1,12 @@
+#For python3
+#This method includes complex numbers for a quadratic equation
 import math
 print('This progamm is for sovling the quadratic equation')
 P = {'a':0, 'b':0, 'c':0}
 for i in P:
     print('Enter:', i)
     P[i] = int(input())
-D = P['b'] ** 2 - 4 * P['a'] * P['c']
+D = P['b'] ** 2 - 4 * P['a'] * P['c'] #Figure out a disriminant
 if D > 0:
     D = round(math.sqrt(D), 3)
     x1 = float()
@@ -22,13 +24,13 @@ elif D == 0:
         print('x ->', round(x, 3))
         print('D is', round(D, 3))
 else:
-        x1 = complex()
+        x1 = complex() #Unfortunatly i couldn't round complex numbers, so it is what it is
         x2 = complex()
         Dc = D ** (1/2)
         x1 = (-P['b'] ** 2 + Dc) / (2 * P['a'])
         x2 = (-P['b'] ** 2 - Dc) / (2 * P['a'])
         print('As Discriminant D < 0, root will be complex')
-        print('x1 ->', x1)
+        print('x1 ->', x1) #It will be long numbers
         print('x2 ->', x2)
         print('D is', Dc)
         
