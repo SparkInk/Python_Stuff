9705708766e4af1402482f1d802972f3e4b9f0b9
Yasha Umrikhin <32857343+SparkInk@users.noreply.github.com>
2017-11-11 10:40:40 +0900

    Update Max_number.py

--- Functions/Max_number.py
+++ Functions/Max_number.py
@@ -1,15 +1,8 @@
 #Finds out a maximum number of a sequence
 #P.S. it's for python3
-L = list()
-n = int(input('How many numbers you are going to enter?\n'))
-for i in range(0, n):
-    L.append(int(input('Enter number:\n ')))
-max = 0
-for i in range(0, len(L)):
-    if L[i] > max:
-        max = L[i]
-print('The sequence is', L)
-print('Max number in seq is', max)
-
-
- 
+def max_number(n): #How many numbers you are going to enter? (should be a list)
+    max = 0
+    for i in range(0, len(n)):
+        if n[i] > max:
+            max = n[i]
+    return max
