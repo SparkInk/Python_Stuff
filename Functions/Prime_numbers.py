bd5b1164fb80e97deb080db2824b3e777c7b64e1
Yasha Umrikhin <32857343+SparkInk@users.noreply.github.com>
2017-11-11 10:30:34 +0900

    Update Prime_numbers.py

--- Functions/Prime_numbers.py
+++ Functions/Prime_numbers.py
@@ -1,16 +1,17 @@
 #This algorithm is based on The Sieve of Eratosthenes
 # it's for finding all prime numbers up to any given limit
-n = int(input('Enter a number up to calculate: '))
-L = list()
-L1 = list()
-for i in range(2, n):
-    L.append(i)
-for p in L:
-	L1.clear()
-	for i in L:
-		L1.append(p * i)
-	for x in L:        #Could be slow if over 1000
-		for y in L1:
-			if x == y:
-				L.remove(x)
-print('Prime numbers up to', n, 'is:\n', L)
+def prime_numbers(n): 
+    L = list()
+    L1 = list()
+    for i in range(2, n): # filling up a list with natural numbers
+        L.append(i)
+    for p in L:
+	    L1.clear()  
+	    for i in L:
+		    L1.append(p * i)
+	    for x in L:        #Could be slow if over 1000
+		    for y in L1:
+			    if x == y:
+				    L.remove(x)
+    print('Prime numbers up to', n, 'is:')
+    return L
