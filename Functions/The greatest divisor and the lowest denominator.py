197c44fc299cbcd6e17a1817a9d06320098d0005
Iasha Umrikhin <32857343+SparkInk@users.noreply.github.com>
2022-02-11 15:21:21 -0800

    Update The greatest divisor and the lowest denominator.py

--- Functions/The greatest divisor and the lowest denominator.py
+++ Functions/The greatest divisor and the lowest denominator.py
@@ -8,15 +8,19 @@ def divisors(num):
     return s
 
 def gcd_and_lcd(num1, num2):
-    """to find divisors and convert to set"""
+    
+    # to find divisors and convert to set
     s_num1 = set(divisors(num1)) 
     s_num2 = set(divisors(num2))
-    """finding gcd and lcd"""
+    
+    # finding gcd and lcd
     gcd = max(s_num1 & s_num2)
     lcd = (num1 * num2) / gcd
+    
     print("The greatest common divisor is", gcd)
     print("The lowest common denominator is", lcd)
     print('Divisors of ', num1, ' and ', num2, 'is')
     print(divisors(num1), '\n and \n', divisors(num2) )
+    
     return None
     
